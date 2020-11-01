069a50313175acb8288bb664717e7bdafcca4d9b
Auros Nexus <aurosnex@gmail.com>
2020-11-01 14:17:34 -0500

    Handle logic in separate class with a weak association to the button
    Although I'd love to have a physical component directly on the detail view, that would violate the principle rule of `View`s, in which they themselves don't have their dependencies injected.

---
+++ DiDetails/Managers/DetailContextManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Zenject;
+using DiDetails.Utilities;
+using UnityEngine.EventSystems;
+using BeatSaberMarkupLanguage.Components;
+
+namespace DiDetails.Managers
+{
+    internal sealed class DetailContextManager : IInitializable, IDisposable
+    {
+        private readonly ClickableImage _artworkImage;
+        private readonly StandardLevelDetailViewController _standardLevelDetailViewController;
+
+        #region Initialization
+
+        internal DetailContextManager(StandardLevelDetailViewController standardLevelDetailViewController)
+        {
+            _standardLevelDetailViewController = standardLevelDetailViewController;
+
+            // Let's ref the image that we already upgraded in our Zenjector
+            var detailView = Accessors.DetailView(ref _standardLevelDetailViewController);
+            var levelBar = Accessors.LevelBar(ref detailView);
+            _artworkImage = (ClickableImage)Accessors.Artwork(ref levelBar);
+        }
+
+        public void Initialize()
+        {
+            _artworkImage.OnClickEvent += ArtworkImageClicked;
+        }
+
+        public void Dispose()
+        {
+            _artworkImage.OnClickEvent -= ArtworkImageClicked;
+        }
+
+        #endregion
+
+        private void ArtworkImageClicked(PointerEventData pointerEventData)
+        {
+            
+        }
+    }
+}
--- DiDetails/Plugin.cs
+++ DiDetails/Plugin.cs
@@ -19,21 +19,19 @@ namespace DiDetails
         {
             Log = logger;
 
+            // Register our Installer
             zenjector.OnMenu<DiDMenuInstaller>()
                 .Mutate<StandardLevelDetailViewController>((ctx, obj) =>
                 {
+                    // Get the ImageView reference on the view controller
                     var viewController = obj as StandardLevelDetailViewController;
                     var detailView = Accessors.DetailView(ref viewController);
                     var levelBar = Accessors.LevelBar(ref detailView);
                     var artwork = Accessors.Artwork(ref levelBar);
 
+                    // Upgrade the ImageView
                     var clickable = artwork.Upgrade<ImageView, ClickableImage>();
                     Accessors.Artwork(ref levelBar) = clickable;
-
-                    clickable.OnClickEvent += (p) =>
-                    {
-                        Log.Info("Clicked Image");
-                    };
                 });
         }
 
